1445a3b2bd3ba871c220f5144c71587beef99309
APPPamelavidal <newhartzshop@gmail.com>
2026-06-13 14:01:04 -0300

    app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,29 @@
+function cadastrar() {
 
+    const nome = document.getElementById("nome").value;
+    const celular = document.getElementById("celular").value;
+    const senha = document.getElementById("senha").value;
+    const confirmarSenha = document.getElementById("confirmarSenha").value;
+
+    if(!nome || !celular || !senha || !confirmarSenha){
+        alert("Preencha todos os campos");
+        return;
+    }
+
+    if(senha !== confirmarSenha){
+        alert("As senhas não conferem");
+        return;
+    }
+
+    const usuario = {
+        nome,
+        celular,
+        senha
+    };
+
+    localStorage.setItem("usuario", JSON.stringify(usuario));
+
+    alert("Cadastro realizado com sucesso!");
+
+    window.location.href = "login.html";
+}
